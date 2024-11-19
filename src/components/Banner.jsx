3919db31e8b811427003fe83da96349fa9e0e35b
Mahajabin Tokon <tokonislam1122@gmail.com>
2024-11-18 18:49:06 -0700

    Banner section complete

---
+++ src/components/Banner.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import bp1 from "../assets/bp1.png"
+import bp2 from "../assets/bp2.png"
+import bp3 from "../assets/bp3.png"
+
+const Banner = () => {
+  return (
+    <div className="carousel w-full">
+        
+      <div id="slide1" className="carousel-item relative w-full">
+        <img
+          src={bp1}
+          className="w-full h-96"
+        />
+        <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+          <a href="#slide3" className="btn btn-circle">
+            ❮
+          </a>
+          <a href="#slide2" className="btn btn-circle">
+            ❯
+          </a>
+        </div>
+      </div>
+      <div id="slide2" className="carousel-item relative w-full">
+        <img
+          src={bp2}
+          className="w-full h-96"
+        />
+        <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+          <a href="#slide1" className="btn btn-circle">
+            ❮
+          </a>
+          <a href="#slide3" className="btn btn-circle">
+            ❯
+          </a>
+        </div>
+      </div>
+      <div id="slide3" className="carousel-item relative w-full">
+        <img
+          src={bp3}
+          className="w-full h-96"
+        />
+        <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+          <a href="#slide2" className="btn btn-circle">
+            ❮
+          </a>
+          <a href="#slide1" className="btn btn-circle">
+            ❯
+          </a>
+        </div>
+      </div>
+      
+    </div>
+  );
+};
+
+export default Banner;
--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React from "react";
+import Banner from "../Banner";
 
 const Home = () => {
-    return (
-        <div>
-            I am home
-        </div>
-    );
+  return (
+    <div>
+      <div className="max-w-6xl mx-auto my-4 px-2">
+        <Banner></Banner>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
