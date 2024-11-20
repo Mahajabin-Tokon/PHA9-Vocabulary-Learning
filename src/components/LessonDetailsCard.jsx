2f8d32b112a436b49a7d158872576cc6d3784dad
Mahajabin Tokon <tokonislam1122@gmail.com>
2024-11-19 19:01:15 -0700

    Working on word card

--- src/components/LessonDetailsCard.jsx
+++ src/components/LessonDetailsCard.jsx
@@ -1,13 +1,29 @@
-import React from 'react';
+import React from "react";
 
-const LessonDetailsCard = ({vocabulary}) => {
-    const {id, word, pronunciation, meaning, part_of_speech, difficulty, lesson_no, when_to_say, example} = vocabulary
-    console.log(vocabulary)
-    return (
-        <div>
-            {vocabulary.id}
-        </div>
-    );
+const LessonDetailsCard = ({ vocabulary }) => {
+  const {
+    id,
+    word,
+    pronunciation,
+    meaning,
+    part_of_speech,
+    difficulty,
+    lesson_no,
+    when_to_say,
+    example,
+  } = vocabulary;
+//   Word, meaning, pronunciation, part_of_speech
+
+  return (
+    <div className={`flex flex-col justify-center items-start border-2 p-5 font-semibold gap-2 ${difficulty==="easy"?"bg-green-200":""} ${difficulty==="medium"?"bg-orange-200":""} ${difficulty==="difficult"?"bg-red-200":""}`}>
+      <p className="">Word: {word}</p>
+      <p className="">Meaning: {meaning}</p>
+      <p className="">Pronunciation: {pronunciation}</p>
+      <p className="">Part of speech: {part_of_speech}</p>
+      <button className="btn w-full">When to say</button>
+      <button className="btn w-full">Pronouce</button>
+    </div>
+  );
 };
 
 export default LessonDetailsCard;
--- src/components/Pages/LessonDetails.jsx
+++ src/components/Pages/LessonDetails.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useLoaderData, useParams } from "react-router-dom";
+import { Link, useLoaderData, useParams } from "react-router-dom";
 import LessonDetailsCard from "../LessonDetailsCard";
 
 const LessonDetails = () => {
@@ -16,13 +16,23 @@ const LessonDetails = () => {
   }, [data, lessonNumFromURL]);
 
   return (
-    <div>
-      {vocabs.map((eachVocabulary) => (
-        <LessonDetailsCard
-          key={eachVocabulary.id}
-          vocabulary={eachVocabulary}
-        ></LessonDetailsCard>
-      ))}
+    <div className="max-w-6xl mx-auto my-10 px-2">
+      <div className="text-center my-10">
+        <p className="text-center text-4xl font-bold">
+          Lessons {lessonNumFromURL.lesson_no}
+        </p>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 my-2 p-2">
+        {vocabs.map((eachVocabulary) => (
+          <LessonDetailsCard
+            key={eachVocabulary.id}
+            vocabulary={eachVocabulary}
+          ></LessonDetailsCard>
+        ))}
+      </div>
+      <div className="text-center my-10">
+        <Link to="/learning" className="btn w-full font-bold">Back to Lesson</Link>
+      </div>
     </div>
   );
 };
